5ec5acec99274c3dae345028b07652a2db160836
mbron770 <mbronfin.tsd@gmail.com>
2023-10-04 16:24:09 -0400

    first commit

---
+++ index.py
@@ -0,0 +1,42 @@
+from flask import Flask, make_response, jsonify, request
+from flask_migrate import Migrate
+from http.server import BaseHTTPRequestHandler
+from database import db
+from flask_restful import Api, Resource
+from flask_cors import CORS
+from models import User
+import os
+
+app = Flask(__name__)
+CORS(app)
+api = Api(app)
+app.secret_key="hello"
+SESSION_TYPE="sqlalchemy"
+# DB_URL = os.environ.get('DB_URL')
+# DB_URL="postgresql://default:0kfNebo1Twgp@ep-hidden-rain-16108082.us-east-1.postgres.vercel-storage.com:5432/verceldb"
+DB_URL="postgresql://mbron770:14EXbZiTMBtz@ep-black-cherry-71826577.us-east-2.aws.neon.tech/neondb"
+app.config['SQLALCHEMY_DATABASE_URI'] = DB_URL
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
+migrate = Migrate(app, db)
+db.init_app(app)
+
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+
+@app.route('/test',  methods = ['GET', 'PATCH'])
+def test():
+    if(request.method == 'GET'):
+        all = User.query.all()
+        users = []
+        for user in all:
+            users.append(str(user))
+        return jsonify(users), 200
+
+
+    # if __name__ == '__main__':
+    #     app.run()
+
